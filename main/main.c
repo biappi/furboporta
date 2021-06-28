08a40828401ecadc519bd26d4015a7b2e019554f
Antonio Malara <antonio.malara@gmail.com>
2021-06-28 23:55:16 +0200

    notify motion for 10 seconds when buttons pressed

--- main/main.c
+++ main/main.c
@@ -8,6 +8,7 @@
 #include <esp_log.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
+#include <freertos/timers.h>
 #include <driver/gpio.h>
 
 #include "segreto-di-fatima.h"
@@ -20,12 +21,16 @@ const char* ACCESSORY_MODEL        = "FRB-002";
 const char* ACCESSORY_SERIAL_NR    = "0";
 const char* ACCESSORY_REVISION     = "R1";
 
+const int   MOTION_STOP_TIMEOUT    = 10000; // ms
+
 typedef struct {
-    const char *name;
-    int        button_pin;
-    int        value;
-    debounce_t debounce;
-    void       *chr;
+    const char    *name;
+    int           button_pin;
+    int           value;
+    debounce_t    debounce;
+    void          *chr;
+    TimerHandle_t motion_stop_timer;
+    bool          needs_to_clear;
 } sensor_t;
 
 sensor_t sensors[] = {
@@ -86,28 +91,54 @@ esp_err_t name_read_3(hk_mem *response) {
 }
 
 //
+void sensor_timer_callback(TimerHandle_t timer);
 
 void sensor_init(sensor_t *sensor) {
     gpio_pad_select_gpio(sensor->button_pin);
     gpio_set_direction(sensor->button_pin, GPIO_MODE_INPUT);
 
     sensor->debounce.inverted = true;
+
+    sensor->motion_stop_timer = xTimerCreate(
+        "motion stop timeout",
+        pdMS_TO_TICKS(MOTION_STOP_TIMEOUT),
+        pdFALSE,
+        sensor,
+        sensor_timer_callback
+    );
+
+    assert(sensor->motion_stop_timer);
 }
 
+static void sensor_set_value(sensor_t *sensor, int value) {
+    sensor->value = value;
+    hk_notify(sensor->chr);
+    printf("notifying new value: %s: %d\n", sensor->name, sensor->value);
+}
 
 void sensor_update(sensor_t *sensor) {
     int level = gpio_get_level(sensor->button_pin);
     debounce_update(&sensor->debounce, level);
 
+    if (sensor->needs_to_clear) {
+        sensor_set_value(sensor, 0);
+        sensor->needs_to_clear = false;
+    }
+
     if (debounce_down(&sensor->debounce)) {
-        sensor->value = !sensor->value;
-        hk_notify(sensor->chr);
-        printf("%s down, new value %d\n", sensor->name, sensor->value);
+        printf("%s down\n", sensor->name);
+        sensor_set_value(sensor, 1);
+        xTimerReset(sensor->motion_stop_timer, 10);
     }
 
     if (debounce_up(&sensor->debounce)) {
         printf("%s up\n", sensor->name);
     }
+}
+
+void sensor_timer_callback(TimerHandle_t timer) {
+    sensor_t *sensor = pvTimerGetTimerID(timer);
+    sensor->needs_to_clear = true;
 }
 
 //
